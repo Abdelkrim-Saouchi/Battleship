ba88441523914bd1c727e11044747f9d5c2f6c16
Krimothiazine <krimouv1995@gmail.com>
2023-04-27 13:00:15 +0100

    Install env

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship',
+    }),
+  ],
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
